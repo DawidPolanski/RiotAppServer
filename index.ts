939da04f9951a653c989573af30b39cb312a746d
DawidPolanski <dpolanski1999@gmail.com>
2024-04-25 14:24:51 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,53 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+const API_KEY = process.env.API_KEY;
+const region = "EUN1";
+
+app.get("/summonerData/:gameName/:tagLine", async (req, res) => {
+  const { gameName, tagLine } = req.params;
+
+  try {
+    const encodedTagLine = encodeURIComponent(tagLine);
+
+    const response = await axios.get(
+      `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${encodedTagLine}`,
+      { headers: { "X-Riot-Token": API_KEY } }
+    );
+    const summonerData = response.data;
+    res.json(summonerData);
+  } catch (error) {
+    console.error("Error fetching summoner data:", error.message);
+    res.status(500).json({ error: "Error fetching summoner data" });
+  }
+});
+
+app.get("/summonerMatch/:puuid", async (req, res) => {
+  const { puuid } = req.params;
+
+  try {
+    const response = await axios.get(
+      `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20`,
+      { headers: { "X-Riot-Token": API_KEY } }
+    );
+    const summonerMatch = response.data;
+    res.json(summonerMatch);
+  } catch (error) {
+    console.error("Error fetching summoner match data:", error.message);
+    res.status(500).json({ error: "Error fetching summoner match data" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
