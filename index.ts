cd7e58b1cbc8f6c7034680eeefc715921885d039
DawidPolanski <dpolanski1999@gmail.com>
2024-04-26 14:13:05 +0200

    Refactor endpoint routes for improved readability and efficiency

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
@@ -11,19 +9,23 @@ app.use(cors());
 
 const API_KEY = process.env.API_KEY;
 
-// const region = "EUN1";
-
-app.get("/summonerData/:gameName/:tagLine", async (req, res) => {
-  const { gameName, tagLine } = req.params;
+app.get("/summonerData", async (req, res) => {
+  const { summoners } = req.query;
 
   try {
-    const encodedTagLine = encodeURIComponent(tagLine);
+    const promises = summoners.map(async (summoner) => {
+      const { gameName, tagLine } = summoner;
+      const encodedTagLine = encodeURIComponent(tagLine);
 
-    const response = await axios.get(
-      `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${encodedTagLine}`,
-      { headers: { "X-Riot-Token": API_KEY } }
-    );
-    const summonerData = response.data;
+      const response = await axios.get(
+        `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${encodedTagLine}`,
+        { headers: { "X-Riot-Token": API_KEY } }
+      );
+
+      return response.data;
+    });
+
+    const summonerData = await Promise.all(promises);
     res.json(summonerData);
   } catch (error) {
     console.error("Error fetching summoner data:", error.message);
@@ -31,16 +33,21 @@ app.get("/summonerData/:gameName/:tagLine", async (req, res) => {
   }
 });
 
-app.get("/summonerMatch/:puuid", async (req, res) => {
-  const { puuid } = req.params;
+app.get("/summonerMatch", async (req, res) => {
+  const { puuids } = req.query;
 
   try {
-    const response = await axios.get(
-      `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=30`,
-      { headers: { "X-Riot-Token": API_KEY } }
-    );
-    const summonerMatch = response.data;
-    res.json(summonerMatch);
+    const promises = puuids.map(async (puuid) => {
+      const response = await axios.get(
+        `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=100`,
+        { headers: { "X-Riot-Token": API_KEY } }
+      );
+
+      return response.data;
+    });
+
+    const summonerMatches = await Promise.all(promises);
+    res.json(summonerMatches);
   } catch (error) {
     console.error("Error fetching summoner match data:", error.message);
     res.status(500).json({ error: "Error fetching summoner match data" });
@@ -55,6 +62,7 @@ app.get("/specificMatch/:matchId", async (req, res) => {
       `https://europe.api.riotgames.com/lol/match/v5/matches/${matchId}`,
       { headers: { "X-Riot-Token": API_KEY } }
     );
+
     const specificMatch = response.data;
     res.json(specificMatch);
   } catch (error) {
